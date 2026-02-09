d791adb4e4863a7e29c51dfffe3431497bfe552e
Leonid Budkov <leonid.budkov@gmail.com>
2026-02-09 14:31:59 +0100

    chore: contact - about - projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import About from "../components/sections/About";
 import Hero from "../components/sections/Hero";
+import Projects from "../components/sections/Projects";
+import { projects } from "../data/projects";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Projects/>
+      <About/>
     </main>
   );
 }
---
+++ src/components/about/Skills.tsx
@@ -0,0 +1,32 @@
+export default function Skills () {
+    return (
+        <div className="skills">
+            <div className="skills-group">
+                <h3 className="skils-title">Frontend</h3>
+                <ul>
+                    <li>Next.js</li>
+                    <li>React</li>
+                    <li>TypeScript</li>
+                    <li>Modern CSS</li>
+                </ul>
+            </div>
+            <div className="skills-group">
+                <h3 className="skils-title">Tools</h3>
+                <ul>
+                    <li>GitHub</li>
+                    <li>Figma</li>
+                </ul>
+            </div>
+            <div className="skills-group">
+                <h3 className="skils-title">Frontend</h3>
+                <ul>
+                    <li>WordPress (Headless)</li>
+                    <li>Cutsom Post Types</li>
+                    <li>ACF</li>
+                    <li>REST APIs</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
