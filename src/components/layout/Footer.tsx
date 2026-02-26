1562beaa4ecb6b78ce315c2604fb0716b943d0c4
Leonid Budkov <leonid.budkov@gmail.com>
2026-02-26 13:58:14 +0100

    chore: add basic header and footer [not finished yet]

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,13 @@
+import Container from "./Container";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="footer">
+      <Container>
+        <div className="inner">
+          <p>© {new Date().getFullYear()} Leonid Design. All rights reserved.</p>
+        </div>  
+      </Container>
+    </footer>
+  ) 
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,32 @@
+import { navLink } from "@/src/types/navLinks";
+import Link from "next/link";
+import Container from "../layout/Container";
+import styles from "./Header.module.css";
+
 export default function Header() {
-  return <></>;
-}
+
+    const links: navLink[] = [
+        { href: "#projects", label: "Projects" },
+        { href: "#about", label: "About" },
+        { href: "#contact", label: "Contact" },
+
+    ];
+
+    return (
+        <nav className="navbar">
+            <Container>
+                <div className={styles.inner}>
+                    <img src="/logo.svg" alt="Leonid Design" className={styles.logo} />
+                    <ul className={styles.navLinks}>
+                        {links.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.href}>{link.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <Link href={"#contact"} className={`${styles.navCta} btn btnSecondary`}>CONTACT</Link>
+                </div>
+            </Container>
+        </nav>
+    )
+} 
