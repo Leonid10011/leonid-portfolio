456e5a0e0585c7d01d7bd3a569fd903220191b56
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-16 12:34:50 +0100

    feat: add freelance-board rewrite route

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lbcoding.com', 
-        port: '',
-        pathname: '/wp-content/uploads/**', // Optional: Erhöht die Sicherheit
+        protocol: "https",
+        hostname: "lbcoding.com",
+        port: "",
+        pathname: "/wp-content/uploads/**", // Optional: Erhöht die Sicherheit
       },
     ],
   },
